7d32307057ef0d6fc3e09977e504e5ce160183ea
debjyotich <86590192+debjyotich@users.noreply.github.com>
2021-07-02 09:45:42 +0530

    Update lab_code_1_day_2.ino

--- lab_code_1_day_2.ino
+++ lab_code_1_day_2.ino
@@ -14,8 +14,8 @@
 
 ConsentiumThings link;   // create ConsentiumThing object
 
-const char *ssid = "DebjyotiFi"; // add WiFi SSID
-const char *pass = "cxa1619s"; // add WiFi password
+const char *ssid = "guest"; // add WiFi SSID
+const char *pass = "guest123"; // add WiFi password
  
 const char *ip = "consentiuminc.online";  // Consentium server address
 const char *port = "80"; // Consentium server port
@@ -30,14 +30,9 @@ void setup(){
 void loop(){
   float data_0 = analogRead(A0)*0.488; // Example sensor data read from A0
   float data_1 = analogRead(A1)*0.488; // Example sensor data read from A1
-  float data_2 = analogRead(A2)*0.488; // Example sensor data read from A2
-  float data_3 = analogRead(A3)*0.488; // Example sensor data read from A3
-  float data_4 = analogRead(A4)*0.488; // Example sensor data read from A4
-  float data_5 = analogRead(A5)*0.488; // Example sensor data read from A5
-  float data_6 = 32.1;
   
-  float sensor_val[] = {data_0, data_1, data_2, data_3, data_4, data_5, data_6};  // sensor data array
-  String info[] = {"Temperature", "Pressure", "RPM", "Current", "Voltage", "Vibration", "Ramdom"}; // sensor info. array
+  float sensor_val[] = {data_0, data_1};  // sensor data array
+  String info[] = {"Temperature", "Pressure"}; // sensor info. array
   
   int sensor_num = sizeof(sensor_val)/sizeof(sensor_val[0]); // number of sensors connected 
   
